b56599dac46c4ac4c395c6f2175f2decdc4f207d
Anthony Corletti <anthcor@gmail.com>
2022-12-13 16:26:16 -0500

    doc

--- hudson/client/main.py
+++ hudson/client/main.py
@@ -1,3 +1,3 @@
-# this is the client that people can import to help them automate the ml workflow
-# then they use the cli to deploy their workflow to run remotely and point to a
-# hudson server – so some basic client configuration code is needed
+# TODO: this is the client that users will import into their project
+# they can the cli to deploy their stuff or run hudson locally
+# a little bit of client config as code is required
